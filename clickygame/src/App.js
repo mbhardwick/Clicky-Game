c27857e326fb30f89c076ca8689ac8000ce24d2b
MB Hardwick <48660923+mbhardwick@users.noreply.github.com>
2019-12-13 18:29:20 -0800

    images render to page

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import Cards from './components/Cards/Cards';
+import Title from './components/Title/Title';
+import Wrapper from './components/Wrapper/Wrapper';
+import cats from './cactors.json';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let score = 0;
+let highScore = 0;
+let message = "Click each cat once to earn points! Be aware, click the same cat twice and you lose!"
+
+class App extends Component {
+  state = {
+    cats,
+    score,
+    highScore,
+    message
+  };
+
+  setClicked = id => {
+    const matches = this.state.matches;
+    const clickedMatch = matches.filter(match => match.id === id);
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Cats the Musical: A Click Game</Title>
+        <h3 className="score">{this.state.score}</h3>
+        <h3 className="score card-header">
+          Score: {this.state.score}
+          <br />
+          High Score: {this.state.highScore}
+        </h3>
+        <div className="container">
+        <div className="row">
+          {this.state.cats.map(match => (
+            <Cards
+              setClicked={this.setClicked}
+              id={match.id}
+              key={match.id}
+              image={match.image}
+            />
+          ))}
+        </div>
+        </div>
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
