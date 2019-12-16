e19661bd030273f6eb637c47e8b2d845c758eb41
MB Hardwick <48660923+mbhardwick@users.noreply.github.com>
2019-12-16 10:26:15 -0800

    score/highscore/shuffle functionality

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Cards from './components/Cards/Cards';
 import Title from './components/Title/Title';
 import Wrapper from './components/Wrapper/Wrapper';
@@ -9,7 +9,7 @@ let score = 0;
 let highScore = 0;
 let message = "Click each cat once to earn points! Be aware, click the same cat twice and you lose!"
 
-class App extends Component {
+class App extends React.Component {
   state = {
     cats,
     score,
@@ -18,9 +18,58 @@ class App extends Component {
   };
 
   setClicked = id => {
-    const matches = this.state.matches;
-    const clickedMatch = matches.filter(match => match.id === id);
-  }
+    const cats = this.state.cats;
+    const clickedCat = cats.filter(cat => cat.id === id);
+  
+
+    if (clickedCat[0].clicked) {
+      console.log("Correct Guesses: " + score);
+      console.log("Highscore: " + highScore);
+
+    score = 0;
+    message = "You already clicked on " + cats.name + "! You lose!";
+
+    for(let i = 0; i<cats.length; i++) {
+      cats[i].clicked = false;
+    }
+
+    this.setState({ message });
+    this.setState({ score });
+    this.setState({ cats });
+
+  } else if (score < 11) {
+    clickedCat[0].clicked = true;
+    score++;
+    message = "Great job! Try another!";
+    
+    if(score > highScore){
+      highScore = score;
+      this.setState({ highScore });
+    }
+
+    cats.sort(function(a, b){return 0.5 - Math.random()});
+
+    this.setState({ cats });
+    this.setState({ score });
+    this.setState({ message });
+  } else {
+    clickedCat[0].clicked = true;
+    score = 0;
+    message = "Congratulations! You got them all! Think you can do it again?";
+    highScore = 12;
+    this.setState({ highScore });
+
+    for (let i=0; i<cats.length; i++){
+      cats[i].clicked= false;
+    }
+
+    cats.sort(function(a, b){return 0.5 - Math.random()});
+
+    this.setState({ cats });
+    this.setState({ score });
+    this.setState({ message });
+    } 
+};
 
   render() {
     return (
@@ -34,12 +83,12 @@ class App extends Component {
         </h3>
         <div className="container">
         <div className="row">
-          {this.state.cats.map(match => (
+          {this.state.cats.map(cat => (
             <Cards
               setClicked={this.setClicked}
-              id={match.id}
-              key={match.id}
-              image={match.image}
+              id={cat.id}
+              key={cat.id}
+              image={cat.image}
             />
           ))}
         </div>
