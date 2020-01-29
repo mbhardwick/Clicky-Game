fc8051238eccedc651331e387e8fc9a1beae4f7d
MB Hardwick <48660923+mbhardwick@users.noreply.github.com>
2020-01-29 13:31:59 -0800

    minimal styling

--- clickygame/src/components/cards/cards.js
+++ clickygame/src/components/cards/cards.js
@@ -3,7 +3,7 @@ import "./Cards.css";
 
 const Cards = props => (
 
-	<div onClick={()=> props.setClicked(props.id)} className="card col-md-3">
+	<div onClick={()=> props.setClicked(props.id)} className="card col-sm-3" id="img-card">
 		<div className="img-container">
 			<img alt={props.name} src={props.image} />
 		</div>
